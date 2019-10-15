7543df7b935fc9141790df0967f89f55bf05fa85
Olga-alizar <54671370+Olga-alizar@users.noreply.github.com>
2019-10-15 17:38:17 +0300

    Add files via upload

---
+++ JavaToMySQL.java
@@ -0,0 +1,65 @@
+package package1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+// Класс для работы с MySQL
+public class JavaToMySQL {
+
+    // JDBC URL, username and password of MySQL server
+    private static final String url = "jdbc:mysql://localhost:3306/library";
+    private static final String user = "admin";
+    private static final String password = "User_mysql0$";
+
+    // JDBC variables for opening and managing connection
+    private static Connection con ;
+    private static Statement stmt;
+    public static ResultSet rs;
+    // Коннект к серверу
+    public static void connectToServer() {
+        try {
+            // открываем коннект к базе данных MySQL server
+            con = DriverManager.getConnection(url, user, password);
+            // getting Statement object to execute query
+            stmt = con.createStatement();
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        }
+    }
+    // Получаем объект для выполнения запроса
+    public static void getStatement() {
+        try {
+            // getting Statement object to execute query
+            stmt = con.createStatement();
+
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        }
+    }
+    // Закрываем подключение к серверу
+    public static void closeconnectToServer() {
+        //закрываем всё
+        try { con.close(); } catch(SQLException se) { /*can't do anything */ }
+        try { stmt.close(); } catch(SQLException se) { /*can't do anything */ }
+        try { rs.close(); } catch(SQLException se) { /*can't do anything */ }
+    }
+
+    // Выролняем запрос или Select или Insert, Update, Delete
+    public static void execQuery(String sQuery, boolean bQuery) {
+        try {
+            if (bQuery) {
+                // executing SELECT query
+                rs = stmt.executeQuery(sQuery);
+            }
+            else {
+                stmt.execute(sQuery);
+            }
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+        }
+    }
+
+}
