18929d9ea2495181a657a65972bbb256c991f9ee
Olga-alizar <54671370+Olga-alizar@users.noreply.github.com>
2019-10-17 21:17:42 +0300

    Add files via upload

--- LibraryManager.java
+++ LibraryManager.java
@@ -9,14 +9,18 @@ import package1.*;
 
 public class LibraryManager {
 
-    private static ArrayList<BooKs> books = new ArrayList<>();
+    protected static ArrayList<Book> books;
+
+    static {
+        books = new ArrayList<>();
+    }
 
     public static void main(String[] args) throws ParseException, SQLException{
         LibraryManager lb = new LibraryManager();
         lb.testLibrary();
     }
     // Получаем список всех книг библиотеки
-    void readBooks()  throws SQLException {
+    private void readBooks()  throws SQLException {
         JavaToMySQL jsql = new JavaToMySQL();
         jsql.connectToServer();
 
@@ -24,16 +28,16 @@ public class LibraryManager {
                 "left join author a on a.id = b.author_id", true);
         books.clear();
         while (jsql.rs.next()) {
-            BooKs b1=new BooKs();
-            b1.id = jsql.rs.getInt("id");
-            b1.title = jsql.rs.getString("bookName");
-            b1.ISBN = jsql.rs.getString("ISBN");
-            b1.genre = jsql.rs.getString("genre");
-            b1.printDate = jsql.rs.getDate("printDate");
+            Book b1=new Book();
+            b1.setId(jsql.rs.getInt("id"));
+            b1.setBookName( jsql.rs.getString("bookName"));
+            b1.setISBN( jsql.rs.getString("ISBN"));
+            b1.setGenre( jsql.rs.getString("genre"));
+            b1.setPrintDate( jsql.rs.getDate("printDate"));
             b1.author = new Author();
-            b1.author.id = jsql.rs.getInt("author_id");
-            b1.author.authorName = jsql.rs.getString("authorName");
-            b1.author.birthDate = jsql.rs.getDate("birthDate");
+            b1.author.setId( jsql.rs.getInt("author_id"));
+            b1.author.setName(jsql.rs.getString("authorName"));
+            b1.author.setBirthDate( jsql.rs.getDate("birthDate"));
             books.add(b1);
             System.out.println(b1.toString());
             b1 = null;
@@ -42,38 +46,38 @@ public class LibraryManager {
     }
 
     // тестируем библиотеку
-    void testLibrary() throws ParseException, SQLException{
+    private void testLibrary() throws ParseException, SQLException{
         // Заполняем библиотеку
         Author a1=new Author();
-        BooKs b1=new BooKs();
+        Book b1=new Book();
 
         Genre g1 = Genre.SCIENCE;
         a1.addAuthor("Author1","1953-01-01");
-        b1.addBook("Name1", a1, g1.toString(), "Number1", "2013-01-15");
+        b1.addBook("Name1", a1, g1, "Number1", "2013-01-15");
         a1.addAuthor("Author2","1963-02-02");
-        b1.addBook("Name2", a1, Genre.BELLETRE.toString(), "Number2", "2014-01-15");
+        b1.addBook("Name2", a1, Genre.BELLETRE, "Number2", "2014-01-15");
         a1.addAuthor("Author3","1973-03-03");
-        b1.addBook("Name3", a1, Genre.PHANTASY.toString(), "Number3", "2015-01-15");
+        b1.addBook("Name3", a1, Genre.PHANTASY, "Number3", "2015-01-15");
         a1.addAuthor("Author4","1983-04-04");
-        b1.addBook("Name4", a1, Genre.SCIENCE_FICTION.toString(), "Number4", "2016-01-15");
+        b1.addBook("Name4", a1, Genre.SCIENCE_FICTION, "Number4", "2016-01-15");
         a1.addAuthor("Author4","1983-04-04");
-        b1.addBook("Name5", a1, Genre.SCIENCE_FICTION.toString(), "Number5", "2011-01-15");
+        b1.addBook("Name5", a1, Genre.SCIENCE_FICTION, "Number5", "2011-01-15");
         a1.addAuthor("О.Бузова","1999-01-01");
-        b1.addBook("Цена счастья", a1, Genre.PHANTASY.toString(), "I9HB8BBK", "2010-01-01");
+        b1.addBook("Цена счастья", a1, Genre.PHANTASY, "I9HB8BBK", "2010-01-01");
         a1.addAuthor("В.С. Романчик","1959-11-01");
-        b1.addBook("Java. Методы программирования", a1, Genre.Textbook.toString(), "978-985-7058-30-3", "2013-01-01");
+        b1.addBook("Java. Методы программирования", a1, Genre.Textbook, "978-985-7058-30-3", "2013-01-01");
 
         System.out.println("После добавления книг:");
         readBooks();
 
         //Получение книги
-        BooKs get1=new BooKs();
-        BooKs rb = get1.getBook("Name1");
+        Book get1=new Book();
+        Book rb = get1.getBook("Name1");
         System.out.println("Получена книга:");
         System.out.println(rb.toString());
 
-        BooKs db1=new BooKs();
-        db1.deleteGenre(Genre.SCIENCE_FICTION.toString());
+        Book db1=new Book();
+        Book.deleteGenre(Genre.SCIENCE_FICTION.toString());
         System.out.println("После удаления жанра:");
         readBooks();
         // удаление книги Name2
